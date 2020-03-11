ffee8f3edc0c7bd4209c8179078a7dc270ea94e1
KristinaFrants <cristina.chiticari@gmail.com>
2020-03-11 22:33:46 +0000

    first

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
+from models import db, Person
 #from models import Person
 
 app = Flask(__name__)
@@ -27,6 +27,48 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
+@app.route('/person', methods=['POST'])
+def handle_person():
+
+    # First we get the payload json
+    body = request.get_json()
+
+    user1 = Person(username=body['username'], email=body['email'])
+    db.session.add(user1)
+    db.session.commit()
+    return "ok", 200
+
+@app.route('/users', methods=['GET'])
+def handle_users():
+
+    if request.method == 'GET':
+        users = Person.query.all()
+
+        if not users:
+            return jsonify({'msg':'User not found'}), 404
+
+        return jsonify( [x.serialize() for x in users] ), 200
+
+    return "Invalid Method", 404
+
+@app.route('/userstwo/<int:person_id>', methods=['PUT'])
+def handle_update(person_id):
+
+    body = request.get_json()
+    user1 = Person.query.get(person_id)
+
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+    if "username" in body:
+        user1.username = body["username"]
+    if "email" in body:
+        user1.email = body["email"]
+
+    db.session.commit()
+    return "ok", 200
+
+
 
 @app.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
